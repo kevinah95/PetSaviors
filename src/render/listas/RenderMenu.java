905fccc2d40cd5d91f5a82edf139a19904633c15
jm95aguilar <jm95aguilar@hotmail.com>
2014-11-04 12:12:00 -0600

    Paquete render
    Actualiza el contenido de los labels en el menú y la principal

---
+++ src/render/listas/RenderMenu.java
@@ -0,0 +1,37 @@
+package render.listas;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import menu.principal.PrincipalNormal;
+
+public class RenderMenu implements ListCellRenderer{
+	
+		  protected DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+		
+		  public Component getListCellRendererComponent(JList list, Object value, int index,
+		      boolean isSelected, boolean cellHasFocus) {
+			  
+		    JLabel renderer = (JLabel) defaultRenderer.getListCellRendererComponent(list, value, index,
+		        isSelected, cellHasFocus);
+		   
+		    renderer.setBackground(new Color(130, 177, 166));
+		    renderer.setPreferredSize(new Dimension(50, 30));
+		    renderer.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+		    renderer.setBorder(null);
+		    if (!cellHasFocus){
+		    	
+		    }else{
+		    	renderer.setForeground(new Color(255, 255, 255));
+		    	renderer.setBackground(new Color(68, 108, 98));
+		    }
+		    return renderer;
+	}
+}
---
+++ src/render/listas/RenderPrincipal.java
@@ -0,0 +1,37 @@
+package render.listas;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import menu.principal.PrincipalNormal;
+
+public class RenderPrincipal implements ListCellRenderer{
+	
+		  protected DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+		
+		  public Component getListCellRendererComponent(JList list, Object value, int index,
+		      boolean isSelected, boolean cellHasFocus) {
+			  
+		    JLabel renderer = (JLabel) defaultRenderer.getListCellRendererComponent(list, value, index,
+		        isSelected, cellHasFocus);
+		   
+			renderer.setIcon(new ImageIcon(PrincipalNormal.class.getResource("/recursos/punta.png")));
+		    renderer.setBackground(new Color(130, 177, 166));
+		    renderer.setPreferredSize(new Dimension(79, 50));
+		    renderer.setBorder(null);
+		    if (!cellHasFocus){
+		    	renderer.setIcon(null);
+		    }else{
+		    	renderer.setForeground(new Color(255, 255, 255));
+		    	renderer.setBackground(new Color(68, 108, 98));
+		    }
+		    return renderer;
+	}
+}
